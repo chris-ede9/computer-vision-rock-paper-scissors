932dc672590229e5580d5aaccd9d73e13181b6c6
chris-ede9 <chris_ede9@outlook.com>
2023-04-16 14:25:12 +0100

    Added a get_winner function to take the computer choice and a user choice to determine who would be the winner for a single game

--- manual_rps.py
+++ manual_rps.py
@@ -1,7 +1,7 @@
 import random
 
 def get_computer_choice():
-    # Returns a random value from the choices list
+    # Returns a random value from the options list
     options = ["rock", "paper", "scissors"]
     return random.choice(options)
 
@@ -19,3 +19,30 @@ def get_user_choice():
             return choice
         else:
             print("Invalid choice picked!")
+
+def get_winner(computer_choice, user_choice):
+    '''
+    This fuction determines the winner of a Rock, Paper, Scissors game by comparing the computer choice and the user choice
+
+    Returns:
+        Nothing
+    '''
+    if computer_choice.lower() == user_choice.lower():
+        print("It is a tie!")
+    elif computer_choice.lower() == "rock":
+        if user_choice.lower() == "scissors":
+            print("You lost")
+        elif user_choice.lower() == "paper":
+            print("You won!")
+    elif computer_choice.lower() == "paper":
+        if user_choice.lower() == "rock":
+            print("You lost")
+        elif user_choice.lower() == "scissors":
+            print("You won!")
+    elif computer_choice.lower() == "scissors":
+        if user_choice.lower() == "paper":
+            print("You lost")
+        elif user_choice.lower() == "rock":
+            print("You won!")
+    else:
+        print("An error occured")
